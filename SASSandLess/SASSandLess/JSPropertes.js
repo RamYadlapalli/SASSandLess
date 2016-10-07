9d4b4ef8043018e9d10148387876977fce6f1985
RambabuY <rambabu.1990@hotmail.com>
2016-10-07 19:13:24 +0530

    Added JS concepts

---
+++ SASSandLess/SASSandLess/JSPropertes.js
@@ -0,0 +1,22 @@
+﻿var cat = {
+    name:"Test",
+    color:"White"
+//    type:{type1:"teets",type2:"teezsts"}
+}
+
+//cat.name 
+//cat["name"]
+
+//console.log(Object.getOwnPropertyDescriptor(cat,'name'));   
+Object.defineProperty(cat, 'name', { configurable: false });
+Object.defineProperty(cat, 'name', { enumerable: false });
+cat.name = 123;
+/*for(var property in cat)
+{
+    console.log(cat[property]);
+}*/
+console.log(cat);
+//enumarable 
+//configurable
+//cat.name = "test123";
+//console.log(cat);   
---
+++ SASSandLess/SASSandLess/JavaScript1.js
@@ -0,0 +1,48 @@
+﻿//var cat = { Name: "Cat1", Color: "Black" }
+
+
+/*
+function Cat2() {
+    this.Name = "test1";
+    this.Color = "White";
+}
+
+var cat1 =  new Cat2();
+console.log(window.Color);
+//window.
+*/
+/*
+var cat = Object.create(Object.prototype,
+    {
+        name: {
+            value: "Test"
+        },
+        color: {
+            value: "Test2"
+        }
+    });
+    //cat1.Color = "Stte";
+console.log(cat);
+*/
+
+/*
+class Cat{
+    constructor(a,b){
+        this.a=a;
+        this.b=b;
+    }
+    display(){
+        console.log("in method");
+    }
+}
+
+var cat = new Cat(1,2);
+cat.display();
+*/
+//console.log(cat);
+/*
+var obj = {Name:"Test",Color:"White"};
+console.log(obj);
+obj.Name = 1356;
+console.log(obj);
+*/
